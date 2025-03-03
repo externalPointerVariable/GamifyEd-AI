84bdeffee096b48ece625e3e645c6c3126103b8e
externalPointerVariable <vicky986847@gmail.com>
2025-03-03 14:35:13 +0000

    Add cloud project configuration and quiz generation functionality

--- app/config/config.py
+++ app/config/config.py
@@ -5,3 +5,5 @@ import os
 load_dotenv()
 
 podcastGeneratorApi = os.getenv('APP_VOICE_GENERTOR')
+cloudProjectId = os.getenv('GOOGLE_PROJECT_ID')
+cloudProjectLocation = os.getenv('GOOGLE_PROJECT_LOCATION')
--- app/services/quizGen.py
+++ app/services/quizGen.py
@@ -1,3 +1,36 @@
+import vertexai
+from vertexai.preview.generative_models import GenerativeModel
+import json
+from config.config import cloudProjectId, cloudProjectLocation
+
 class quizGen:
     def __init__(self):
-        pass
+        self.client = vertexai.init(cloudProjectId, cloudProjectLocation)
+        self.topics = []
+        self.difficulties = ['Easy', 'Medium', 'Hard']
+        self.model = GenerativeModel('gemini-pro')
+
+    def generatePracticeQuiz(self):
+        prompt = f'''
+            Generate a quiz based on the following topics:
+            {self.topics}
+            and each questions shouls be generated with one of the following difficulties:
+            {self.difficulties}
+            the format in which the questions should be generated is:[
+            {{
+                "question:": "your question here",
+                "difficulty": "assigned difficulty here",
+                "options": ["option1", "option2", "option3", "option4"],
+                "answer": "correct answer here",
+            }}
+            ]
+            generate the quiz in JSON format
+        '''
+        response = self.model.generate_content(prompt)
+        return json.loads(response)
+    
+
+if __name__ == '__main__':
+    quiz = quizGen()
+    quiz.topics = ['Maths', 'Science', 'History']
+    print(quiz.generatePracticeQuiz())
